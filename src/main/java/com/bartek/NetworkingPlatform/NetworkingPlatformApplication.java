bf2c12aaabc2ef6227fbabf2fec4d1b884266365
galileo680 <bartek.wicz@gmail.com>
2025-01-28 22:56:29 +0100

    init

---
+++ src/main/java/com/bartek/NetworkingPlatform/NetworkingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bartek.NetworkingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartek/NetworkingPlatform/NetworkingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartek.NetworkingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
