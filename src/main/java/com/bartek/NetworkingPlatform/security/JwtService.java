6deccf62ae146b0d7c697ff0d656cfdef1f71597
galileo680 <bartek.wicz@gmail.com>
2025-02-05 21:33:30 +0100

    add security configuration

---
+++ src/main/java/com/bartek/NetworkingPlatform/security/JwtService.java
@@ -0,0 +1,79 @@
+package com.bartek.NetworkingPlatform.security;
+
+import com.bartek.NetworkingPlatform.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretJwtString;
+
+    private SecretKey key;
+
+    @PostConstruct
+    private void init(){
+        byte[] keyBytes = secretJwtString.getBytes(StandardCharsets.UTF_8);
+        this.key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+    public String generateToken(User user){
+        String username = user.getEmail();
+        return generateToken(username);
+    }
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token){
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claimsResolver.apply(claims);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractClaims(token, Claims::getExpiration).before(new Date());
+    }
+
+    public String getTokenFromRequest(HttpServletRequest request){
+        String token = request.getHeader("Authorization");
+        if (StringUtils.hasText(token) && StringUtils.startsWithIgnoreCase(token, "Bearer ")){
+            return token.substring(7);
+        }
+        return null;
+    }
+}
