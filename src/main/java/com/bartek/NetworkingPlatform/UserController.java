3a2684061e00847a9428356c32b00f6ad5e7aad6
galileo680 <bartek.wicz@gmail.com>
2025-02-09 21:20:11 +0100

    add UserController

---
+++ src/main/java/com/bartek/NetworkingPlatform/UserController.java
@@ -0,0 +1,42 @@
+package com.bartek.NetworkingPlatform;
+
+import com.bartek.NetworkingPlatform.dto.request.LoginRequest;
+import com.bartek.NetworkingPlatform.dto.request.RegisterRequest;
+import com.bartek.NetworkingPlatform.dto.response.LoginResponse;
+import com.bartek.NetworkingPlatform.service.UserService;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> registerUser(
+            @RequestBody @Valid RegisterRequest registerRequest
+    ) throws MessagingException {
+        userService.registerUser(registerRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(
+            @RequestBody @Valid LoginRequest loginRequest
+    ) {
+        return ResponseEntity.ok(userService.loginUser(loginRequest));
+    }
+
+    @GetMapping("/activate-account")
+    public ResponseEntity<Void> activateAccount(
+            @RequestParam("token") String token
+    ) throws MessagingException {
+        userService.activateAccount(token);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+}
