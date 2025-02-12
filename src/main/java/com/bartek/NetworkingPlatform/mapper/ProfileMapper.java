bc36915b9cfea987ffdda0e60763e895d17e8c32
galileo680 <bartek.wicz@gmail.com>
2025-02-12 20:55:09 +0100

    change returned type in ProfileMapper to ProfileResponseDTO

--- src/main/java/com/bartek/NetworkingPlatform/mapper/ProfileMapper.java
+++ src/main/java/com/bartek/NetworkingPlatform/mapper/ProfileMapper.java
@@ -1,14 +1,15 @@
 package com.bartek.NetworkingPlatform.mapper;
 
-import com.bartek.NetworkingPlatform.dto.ProfileDTO;
+import com.bartek.NetworkingPlatform.dto.request.ProfileCreateDTO;
+import com.bartek.NetworkingPlatform.dto.response.ProfileResponseDTO;
 import com.bartek.NetworkingPlatform.entity.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProfileMapper {
 
-    public ProfileDTO toDTO(Profile profile) {
-        return ProfileDTO.builder()
+    public ProfileResponseDTO toDTO(Profile profile) {
+        return ProfileResponseDTO.builder()
                 .headline(profile.getHeadline())
                 .bio(profile.getBio())
                 .location(profile.getLocation())
