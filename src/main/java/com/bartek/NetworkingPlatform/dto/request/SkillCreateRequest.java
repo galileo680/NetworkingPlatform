ef83c994e53c17ada4f2b97965aa5a13cd329a68
galileo680 <bartek.wicz@gmail.com>
2025-03-03 20:44:59 +0100

    add Skill related dtos

---
+++ src/main/java/com/bartek/NetworkingPlatform/dto/request/SkillCreateRequest.java
@@ -0,0 +1,19 @@
+package com.bartek.NetworkingPlatform.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SkillCreateRequest {
+
+    @NotBlank(message = "Nazwa umiejętności nie może być pusta")
+    @Size(min = 2, max = 50, message = "Nazwa umiejętności musi mieć od 2 do 50 znaków")
+    private String name;
+}
---
+++ src/main/java/com/bartek/NetworkingPlatform/dto/response/SkillResponse.java
@@ -0,0 +1,19 @@
+package com.bartek.NetworkingPlatform.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SkillResponse {
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
