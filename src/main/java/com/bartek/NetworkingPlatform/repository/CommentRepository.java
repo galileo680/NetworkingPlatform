5b4c65f6233ca067139e41abb1eb6572b8fb5b73
galileo680 <bartek.wicz@gmail.com>
2025-03-09 11:26:58 +0100

    add existsByPostIdAndUserId, deleteByPostIdAndUserId methods in LikeRepository | add order by for findByPostId

--- src/main/java/com/bartek/NetworkingPlatform/repository/CommentRepository.java
+++ src/main/java/com/bartek/NetworkingPlatform/repository/CommentRepository.java
@@ -1,12 +1,13 @@
 package com.bartek.NetworkingPlatform.repository;
 
 import com.bartek.NetworkingPlatform.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPostId(Long postId);
+    Page<Comment> findByPostIdOrderByCreatedAtAsc(Long postId, Pageable pageable);
 }
--- src/main/java/com/bartek/NetworkingPlatform/repository/LikeRepository.java
+++ src/main/java/com/bartek/NetworkingPlatform/repository/LikeRepository.java
@@ -2,8 +2,16 @@ package com.bartek.NetworkingPlatform.repository;
 
 import com.bartek.NetworkingPlatform.entity.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    boolean existsByPostIdAndUserId(Long postId, Long userId);
+
+    @Modifying
+    @Query("DELETE FROM Like l WHERE l.post.id = :postId AND l.user.id = :userId")
+    void deleteByPostIdAndUserId(@Param("postId") Long postId, @Param("userId") Long userId);
 }
