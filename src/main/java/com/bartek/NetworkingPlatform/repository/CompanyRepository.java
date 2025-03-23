4be871b6329f1189e016693c1e823290e9d9fefd
galileo680 <bartek.wicz@gmail.com>
2025-03-23 22:41:00 +0100

    update CompanyRepository with new methods

--- src/main/java/com/bartek/NetworkingPlatform/repository/CompanyRepository.java
+++ src/main/java/com/bartek/NetworkingPlatform/repository/CompanyRepository.java
@@ -1,6 +1,8 @@
 package com.bartek.NetworkingPlatform.repository;
 
 import com.bartek.NetworkingPlatform.entity.Company;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,9 @@ import java.util.Optional;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-    Optional<Company> findByName(String name);
+    boolean existsByName(String name);
+    Page<Company> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Page<Company> findByLocationContainingIgnoreCase(String location, Pageable pageable);
+    Page<Company> findByNameContainingIgnoreCaseAndLocationContainingIgnoreCase(
+            String name, String location, Pageable pageable);
 }
