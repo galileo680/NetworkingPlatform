09cbeae02aa9f99a8405b2dc825fba5ba12da160
galileo680 <bartek.wicz@gmail.com>
2025-03-29 23:31:01 +0100

    update ApplicationRepository with new methods

--- src/main/java/com/bartek/NetworkingPlatform/repository/ApplicationRepository.java
+++ src/main/java/com/bartek/NetworkingPlatform/repository/ApplicationRepository.java
@@ -1,13 +1,26 @@
 package com.bartek.NetworkingPlatform.repository;
 
 import com.bartek.NetworkingPlatform.entity.Application;
+import com.bartek.NetworkingPlatform.enums.ApplicationStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
-    List<Application> findByUserId(Long userId);
-    List<Application> findByJobId(Long jobId);
+    Page<Application> findByJobId(Long jobId, Pageable pageable);
+
+    Page<Application> findByJobIdAndStatus(Long jobId, ApplicationStatus status, Pageable pageable);
+
+    Page<Application> findByUserId(Long userId, Pageable pageable);
+
+    Page<Application> findByUserIdAndStatus(Long userId, ApplicationStatus status, Pageable pageable);
+
+    Optional<Application> findByJobIdAndUserId(Long jobId, Long userId);
+
+    boolean existsByJobIdAndUserId(Long jobId, Long userId);
 }
